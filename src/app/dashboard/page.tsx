965521e9df92591e5be6dd4fcf1fdfafc077326d
Wachira2000 <lewiswachira996@gmail.com>
2025-03-31 09:29:38 -0700

    Changes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div className='py-8 bg-gradient-to-b from-gray-900 to-gray-800 px-8'>Dashboard</div>
+    <div className='py-8 bg-gradient-to-b from-gray-900 to-gray-800 px-8 h-8 w-8 text-red-400 mx-auto'>
+        Under Maintenance
+        </div>
   )
 }
 
